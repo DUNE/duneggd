5b4c0bd903d5e473a6b73fae36289acf071be454
Tyler Alion <tylerdalion@gmail.com>
2015-10-07 15:41:25 -0400

    add preliminary setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(name = 'duneggd',
+      version = '0.0',
+      description = 'General Geometry Description',
+      author = 'Tyler Alion',
+      author_email = 'tylerdalion@gmail.com',
+      license = 'GPLv2',
+      url = 'https://github.com/tyleralion/duneggd',
+      package_dir = {'':'python'},
+      #packages = ['gegede', 'gegede.schema', 'gegede.export', 'gegede.examples'],
+      packages = ['duneggd'],
+      # These are just what were developed against.  Older versions may be okay.
+      install_requires=[
+          "pint >= 0.5.1",      # for units
+          "lxml >= 3.3.5",      # for GDML export
+      ],
+      # implicitly depends on ROOT
+      entry_points = {
+          'console_scripts': [
+              'gegede-cli = gegede.main:main',
+              ]
+      }
+              
+  )
+
