04389f8eca567780288feef87e3f64a2df5aabe0
Tyler Alion <tylerdalion@gmail.com>
2015-11-11 01:54:46 -0500

    Add hooks for RPCModBuilder.

---
+++ python/duneggd/fgt/RPCMod.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+'''
+Subbuilder of RPCTray*Builder
+'''
+
+import gegede.builder
+
+class RPCModBuilder(gegede.builder.Builder):
+    '''
+    Build the RPC modules, the effective unit of the MuID, 
+    constituting an X and Y plane of RPC strips 
+    '''
+
+    #^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^
+    def configure(self,  **kwds):
+
+
+
+
+    #^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^
+    def construct(self, geom):
+
+        # define box and volume for RPC strip
+
+
+        # define box and volume for resistive plate (maybe the same volume for anode and cathode?)
+
+        # define box and volume for whole RPCMod,
+        # to be retrieved by RPCTray*Builder 
+
+
+        # position and place resistive plates in RPCMod
+        
+
+        # for loop to position and place X strips in RPCMod
+
+
+        # for loop to position and place Y strips in RPCMod
+        
+
+        return
