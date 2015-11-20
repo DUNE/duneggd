abf0510da1bb2e3af832d60f15e6d63d3787f67f
Soumya <mailbox.soumya90@gmail.com>
2015-11-20 16:16:24 -0600

    Add ECAL Barrel to Detector

--- python/duneggd/fgt/ECALMod.py
+++ python/duneggd/fgt/ECALMod.py
@@ -18,6 +18,7 @@ class ECALModBuilder(gegede.builder.Builder):
                   ecalThickness = None, 
                   leadThickness = None, 
                   nSBPlanes = None,
+		  altPlaneOrient = True,
                   **kwds):
         if ecalThickness is None:
             raise ValueError("No value given for ecalThickness")
@@ -26,11 +27,11 @@ class ECALModBuilder(gegede.builder.Builder):
         if nSBPlanes is None:
             raise ValueError("No value given for nSBPlanes")
 
-        self.ecalMat       = "Lead"
-        self.ecalThickness = ecalThickness
-        self.leadThickness = leadThickness
-        self.nSBPlanes     = nSBPlanes  
-        
+        self.ecalMat        = "Lead"
+        self.ecalThickness  = ecalThickness
+        self.leadThickness  = leadThickness
+        self.nSBPlanes      = nSBPlanes  
+        self.altPlaneOrient = altPlaneOrient
         self.SBPlaneBldr = self.get_builder('SBPlane')
 
 
@@ -43,17 +44,17 @@ class ECALModBuilder(gegede.builder.Builder):
         
 
         # Calculate ECAL dimensions 
-        self.ecalEndDim    = list(SBPlaneDim) # get the right x and y dimension
-        self.ecalEndDim[2] = self.nSBPlanes*(self.leadThickness + SBPlaneDim[2])
-        print 'ECALEndBuilder: Configured value of ECAL z dimension is '+str(self.ecalThickness)+', assigning calculated value: '+str(self.ecalEndDim[2])
+        self.ecalModDim    = list(SBPlaneDim) # get the right x and y dimension
+        self.ecalModDim[2] = self.nSBPlanes*(self.leadThickness + SBPlaneDim[2])
+        print 'ECALModBuilder: Configured value of ECAL z dimension is '+str(self.ecalThickness)+', assigning calculated value: '+str(self.ecalModDim[2])
       
         # Make main shape/volume for this builder
-        ecalEndBox = geom.shapes.Box( self.name,
-                                      dx=0.5*self.ecalEndDim[0],
-                                      dy=0.5*self.ecalEndDim[1],
-                                      dz=0.5*self.ecalEndDim[2])
-        ecalEnd_lv = geom.structure.Volume('vol'+self.name, material=self.ecalMat, shape=ecalEndBox)
-        self.add_volume(ecalEnd_lv)
+        ecalModBox = geom.shapes.Box( self.name,
+                                      dx=0.5*self.ecalModDim[0],
+                                      dy=0.5*self.ecalModDim[1],
+                                      dz=0.5*self.ecalModDim[2])
+        ecalMod_lv = geom.structure.Volume('vol'+self.name, material=self.ecalMat, shape=ecalModBox)
+        self.add_volume(ecalMod_lv)
   
 
         #Place the SB Planes in the ECAL
@@ -61,7 +62,10 @@ class ECALModBuilder(gegede.builder.Builder):
         n1 = 0 
         n2 = 0
         for i in range(self.nSBPlanes):
-            zpos = -0.5*self.ecalEndDim[2]+ (i+0.5)*SBPlaneDim[2]+(i+1)*self.leadThickness
+            zpos = -0.5*self.ecalModDim[2]+ (i+0.5)*SBPlaneDim[2]+(i+1)*self.leadThickness
+
+	    rotPlane = 'identity'
+	    if(self.altPlaneOrient): rotPlane = 'r90aboutZ'
 
             if i%2==0:
                rsbp_in_ecalend  = geom.structure.Position('SBPlane-'+str(i)+'_in_'+self.name, 
@@ -69,18 +73,18 @@ class ECALModBuilder(gegede.builder.Builder):
                prsbp_in_ecalend = geom.structure.Placement('placeSBPlane-'+str(i)+'_in_'+self.name,
                                                         volume = SBPlane_lv, 
                                                         pos = rsbp_in_ecalend)
-               ecalEnd_lv.placements.append( prsbp_in_ecalend.name )
+               ecalMod_lv.placements.append( prsbp_in_ecalend.name )
                n1=n1+1
             else:
                 rsbp_in_ecalend  = geom.structure.Position('SBPlane-'+str(i)+'_in_'+self.name, 
                                                        '0cm', '0cm', zpos)
                 prsbp_in_ecalend = geom.structure.Placement('placeSBPlane-'+str(i)+'_in_'+self.name,
                                                         volume = SBPlane_lv, 
-                                                        pos = rsbp_in_ecalend, rot="r90aboutZ")
-                ecalEnd_lv.placements.append( prsbp_in_ecalend.name )
+                                                        pos = rsbp_in_ecalend, rot=rotPlane)
+                ecalMod_lv.placements.append( prsbp_in_ecalend.name )
                 n2=n2+1
                 
-        print 'ECALEndBuilder:',i+1 ,'SBPlanes', 'in '+str(self.name)
+        print 'ECALModBuilder:',i+1 ,'SBPlanes', 'in '+str(self.name)
         print n1, 'SBPlanes have scint. bars oriented along X direction and', n2, 'SBPlanes have scint. bars oriented along Y direction for '+str(self.name) 
         return
 
