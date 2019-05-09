1f82a7f409142d28388413ffb4f8f02fe7ab193e
Aran Borkum <aranborkum@hotmail.com>
2019-05-09 16:10:24 +0100

    gegede-example exersize

---
+++ python/duneggd/gegede-example/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name = 'ggdex',
+      version = '0.0',
+      description = 'General Geometry Description Example',
+      author = 'Brett Viren',
+      author_email = 'brett.viren@gmail.com',
+      license = 'GPLv2',
+      url = 'http://github.com/brettviren/gegede-example',
+      packages = ['ggdex'],
+      install_requires=[
+          "gegede >= 0.0",
+      ],
+  )
+
