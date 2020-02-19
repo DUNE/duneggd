025cee8212c92a2dd607e11f221c19e984e088b1
Aran Borkum <aranborkum@hotmail.com>
2020-02-19 09:41:30 +0100

    testing out how to make the field cage

---
+++ python/duneggd/TestArena/FieldCage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+'''
+Subbuilder of the FieldCage
+'''
+
+import gegede.builder
+from gegede import Quantity as Q
+from gegede import units
+
+class FieldCageBuilder(gegede.builder.Builder):
+
